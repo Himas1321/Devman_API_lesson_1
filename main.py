3956d61f9bf92ac5107aac37f7e169db3ac2d0ae
Himas1321 <nikita5045@mail.ru>
2025-09-28 11:43:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import requests
 
 url = 'https://wttr.in/Череповец?MnqT&lang=ru'
-
 response = requests.get(url)
 response.raise_for_status()
+print(response.text)
 
+
+url = 'https://wttr.in/Лондон?MnqT&lang=ru'
+response = requests.get(url)
+response.raise_for_status()
+print(response.text)
+
+
+url = 'https://wttr.in/Шереметьево?MnqT&lang=ru'
+response = requests.get(url)
+response.raise_for_status()
 print(response.text)
